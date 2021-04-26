b2073d264b9cecf5570e0348f70f7bb41c111a5e
J.Hong <tommyix@163.com>
2021-04-26 12:53:56 +0800

    merged
    into ARTubes

--- ARTubes/ARTubes/ViewController.swift
+++ ARTubes/ARTubes/ViewController.swift
@@ -33,31 +33,86 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Set the view's delegate
         sceneView.delegate = self
-
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         sceneView.debugOptions = [SCNDebugOptions.showWireframe, SCNDebugOptions.showFeaturePoints, SCNDebugOptions.showCreases]
-
-        // Create a new scene
-        // let scene = SCNScene(named: "art.scnassets/"+current_tubemodel+".dae")!
-        // let scene = SCNScene(named: "3DModel/ship.scn")!
-//        SCNScene *scene = [SCNScene scenenamed:@'3DModel/']
-
-        // Set the scene to the view
-        // sceneView.scene = scene
     }
     
-    func renderer (_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor){
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        UIApplication.shared.isIdleTimerDisabled = true
+        self.sceneView.autoenablesDefaultLighting = true
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        addGestures()
+        
+    }
+    
+    func addGestures () {
+        let tapped = UITapGestureRecognizer(target: self, action: #selector(tapGesture))
+        
+        sceneView.addGestureRecognizer(tapped)
+    }
+    
+    @objc func tapGesture (sender: UITapGestureRecognizer) {
+        let tapLocation: CGPoint = sender.location(in: sceneView)
+        let estimatedPlane: ARRaycastQuery.Target = .estimatedPlane
+        let alignment: ARRaycastQuery.TargetAlignment = .any
+        let query: ARRaycastQuery? = sceneView.raycastQuery(from: tapLocation, allowing: estimatedPlane, alignment: alignment)
+        if let nonOptQuery: ARRaycastQuery = query {
+            let result: [ARRaycastResult] = sceneView.session.raycast(nonOptQuery)
+            guard let rayCast: ARRaycastResult = result.first
+            else { return }
+            self.loadGeometry(rayCast)
+        }
+    }
+    
+    func loadGeometry(_ result: ARRaycastResult) {
+//        guard let scene = SCNScene(named: "art.scnassets/tube3.dae") else { print("Cannot load SCNScene") ; return }
+//        let node: SCNNode? = scene.rootNode.childNode(withName: "LayerFBXASC0580", recursively: true)
+        guard let scene = SCNScene(named: "art.scnassets/tube3.scn") else { print("Cannot load SCNScene") ; return }
+        let node: SCNNode? = scene.rootNode.childNode(withName: "LayerFBXASC0580", recursively: true)
+        node?.position = SCNVector3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z)
+        self.sceneView.scene.rootNode.addChildNode(node!)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
         let meshNode : SCNNode
         let textNode : SCNNode
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+
+        
         guard let meshGeometry = ARSCNPlaneGeometry(device: sceneView.device!)
-        else {
+            else {
                 fatalError("Can't create plane geometry")
-            }
+        }
         meshGeometry.update(from: planeAnchor.geometry)
         meshNode = SCNNode(geometry: meshGeometry)
         meshNode.opacity = 0.6
@@ -74,94 +129,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         textNode = SCNNode(geometry: textGeometry)
         textNode.name = "TextNode"
-
+        
         textNode.simdScale = SIMD3(repeating: 0.0005)
         textNode.eulerAngles = SCNVector3(x: Float(-90.degreesToradians), y: 0, z: 0)
-
+        
         node.addChildNode(textNode)
+        
         textNode.centerAlign()
         print("did add plane node")
+        
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-        if let planeGeometry = node.childNode(withName: "MeshNode", recursively: false)!.geometry as? ARSCNPlaneGeometry {
-            planeGeometry.update(from: planeAnchor.geometry)
-        }
-    }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+        let planeNode = node.childNode(withName: "MeshNode", recursively: false)
 
-        // Create a session configuration
-        let configuration =  ARWorldTrackingConfiguration()
-        configuration.planeDetection = .horizontal
-        UIApplication.shared.isIdleTimerDisabled = true
-        self.sceneView.autoenablesDefaultLighting = true
-        
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    func addGestures(){
-        let tapped = UITapGestureRecognizer(target: self, action: #selector(tapGesture))
-        sceneView.addGestureRecognizer(tapped)
-    }
-    
-    @objc func tapGesture (sender: UITapGestureRecognizer){
-        let location = sender.location(in: sceneView)
-        let hitTest = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
-        if hitTest.isEmpty{
-            print("No Plane Detected")
-            return
-        }
-        else{
-            let scene = SCNScene(named: "3DModel/ship.scn")!
-            let node = scene.rootNode.childNode(withName: "ship", recursively: false)
-            let columns = hitTest.first?.worldTransform.columns.3
-            node!.position = SCNVector3(x: columns!.x,y: columns!.y, z: columns!.z )
-            sceneView.scene.rootNode.addChildNode(node!)
-            sceneView.scene.rootNode.enumerateChildNodes{ (child, _) in
-                if child.name == "MeshNode" || child.name == "TextNode"{
-                    child.removeFromParentNode()
-                }
+            if let planeGeometry = planeNode?.geometry as? ARSCNPlaneGeometry {
+                planeGeometry.update(from: planeAnchor.geometry)
             }
-        }
+    
     }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-
-        return node
-    }
-*/
-
+    
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-
+        
     }
-
+    
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-
+        
     }
-
+    
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-
+        
     }
 }
+
 
 extension SCNNode {
     func centerAlign() {
@@ -190,5 +195,5 @@ func / (left: SCNVector3, right: Int) -> SCNVector3 {
     return SCNVector3Make(left.x / Float(right), left.y / Float(right), left.z / Float(right))
 }
 extension Int {
-    var degreesToradians : Double {return Double(self) * .pi/100}
+    var degreesToradians : Double {return Double(self) * .pi/180}
 }
