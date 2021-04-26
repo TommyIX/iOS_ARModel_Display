4bfd1e64ca92f87b24968e53f66c6ea5dfeb6cad
J.Hong <tommyix@163.com>
2021-04-27 02:14:56 +0800

    Functional Commit
    增加了显示选择选项

--- ARTubes/ARTubes/ViewController.swift
+++ ARTubes/ARTubes/ViewController.swift
@@ -8,12 +8,18 @@ import ARKit
 import SwiftUI
 
 var current_tubemodel = "none"
+var display_name = false
+var display_type = false
+var display_diameter = false
+var display_coordinate = false
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet weak var Tubes: UIButton!
     
     @IBOutlet var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
     
     @IBAction func Tubes(_ sender: Any) {
         let alertController = UIAlertController(title: "Change the tube", message: "The new selected tube will be selected able to place on the surface", preferredStyle: .actionSheet)
@@ -31,13 +37,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.present(alertController, animated: true, completion: nil)
     }
 
+    @IBAction func Display(_ sender: Any) {
+        let alertController = UIAlertController(title: "Display Settings", message: "Tap to change the display status", preferredStyle: .actionSheet)
+        var nametitle = "Display Tube Name: ON"
+        var typetitle = "Display Tube Type: ON"
+        var dtitle = "Display Diameter: ON"
+        var cotitle = "Display Coordinate: ON"
+        if !display_name {nametitle = "Display Tube Name: OFF"}
+        if !display_type {typetitle = "Display Tube Type: OFF"}
+        if !display_diameter {dtitle = "Display Diameter: OFF"}
+        if !display_coordinate {cotitle = "Display Coordinate: OFF"}
+        
+        let dnameaction = UIAlertAction(title: nametitle, style: .default, handler: {_ in display_name = !display_name})
+        alertController.addAction(dnameaction)
+        let dtypeaction = UIAlertAction(title: typetitle, style: .default, handler: {_ in display_type = !display_type})
+        alertController.addAction(dtypeaction)
+        let daction = UIAlertAction(title: dtitle, style: .default, handler: {_ in display_diameter = !display_diameter})
+        alertController.addAction(daction)
+        let coaction = UIAlertAction(title: cotitle, style: .default, handler: {_ in display_coordinate = !display_coordinate})
+        alertController.addAction(coaction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        if let popoverController = alertController.popoverPresentationController {
+            popoverController.sourceView = self.view
+            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.maxY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        // sceneView.showsStatistics = true
         sceneView.debugOptions = [SCNDebugOptions.showWireframe, SCNDebugOptions.showFeaturePoints, SCNDebugOptions.showCreases]
     }
     
@@ -51,14 +88,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.sceneView.autoenablesDefaultLighting = true
         // Run the view's session
         sceneView.session.run(configuration)
-        
         addGestures()
-        
     }
     
     func addGestures () {
         let tapped = UITapGestureRecognizer(target: self, action: #selector(tapGesture))
-        
         sceneView.addGestureRecognizer(tapped)
     }
     
@@ -74,6 +108,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             self.loadGeometry(rayCast)
         }
     }
+    @IBAction func Reset(_ sender: Any) {
+        sceneView.session.pause()
+        sceneView.scene.rootNode.enumerateChildNodes { (node, stop) in
+            node.removeFromParentNode()
+        }
+        sceneView.session.run(configuration, options : [.resetTracking, .removeExistingAnchors])
+    }
     
     func loadGeometry(_ result: ARRaycastResult) {
         if (current_tubemodel == "none"){
@@ -82,27 +123,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
         guard let scene = SCNScene(named: "art.scnassets/"+current_tubemodel+".scn") else { print("Cannot load SCNScene") ; return }
         let node: SCNNode? = scene.rootNode.childNode(withName: "LayerFBXASC0580", recursively: true)
+        node?.scale = SCNVector3(0.3,0.3,0.3)
         node?.position = SCNVector3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z)
         self.sceneView.scene.rootNode.addChildNode(node!)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         // Pause the view's session
         sceneView.session.pause()
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
+    /*
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
         return node
     }
-*/
+    */
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
@@ -126,7 +165,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         node.addChildNode(meshNode)
         
-        let textGeometry = SCNText(string: "Plane", extrusionDepth: 1)
+        let textGeometry = SCNText(string: "Available Reference Plane", extrusionDepth: 1)
         textGeometry.font = UIFont(name: "Futura", size: 75)
         
         textNode = SCNNode(geometry: textGeometry)
@@ -168,7 +207,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
 }
-
 
 extension SCNNode {
     func centerAlign() {
